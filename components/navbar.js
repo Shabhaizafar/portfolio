7239efb72ded9e5b71843e488e598a603c9ddc18
Shrey K <srkkadia@gmail.com>
2024-03-13 10:15:17 +0530

    redeploy_1

--- components/navbar.js
+++ components/navbar.js
@@ -8,16 +8,16 @@ import {
   Stack,
   Heading,
   Flex,
-  Menu,
-  MenuItem,
-  MenuList,
-  MenuButton,
-  IconButton,
+  // Menu,
+  // MenuItem,
+  // MenuList,
+  // MenuButton,
+  // IconButton,
   useColorModeValue
 } from '@chakra-ui/react'
-import { HamburgerIcon } from '@chakra-ui/icons'
+// import { HamburgerIcon } from '@chakra-ui/icons'
 import ThemeToggleButton from './theme-toggle-button'
-import { IoLogoGithub } from 'react-icons/io5'
+// import { IoLogoGithub } from 'react-icons/io5'
 
 const LinkItem = ({ href, path, target, children, ...props }) => {
   const active = path === href
